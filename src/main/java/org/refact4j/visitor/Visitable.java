4411f5be888a67069785a3c4ba669b873ebac975
Vincent Dupain <vdupain@gmail.com>
2016-01-27 19:16:20 +0100

    Vistable interface inherits Consumer

--- src/main/java/org/refact4j/visitor/Visitable.java
+++ src/main/java/org/refact4j/visitor/Visitable.java
@@ -1,10 +1,10 @@
 package org.refact4j.visitor;
 
+import java.util.function.Consumer;
+
 /**
  * Visitable is an interface for implementing Visitor Design Pattern.
  */
-public interface Visitable {
-
-    void accept(Visitor visitor);
+public interface Visitable extends Consumer<Visitor>{
 
 }
