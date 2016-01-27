a814626d4043e372f2a971758f5e3cd0b6cc855d
Vincent Dupain <vdupain@gmail.com>
2016-01-27 17:53:26 +0100

    suppression tests

--- src/test/java/org/refact4j/functor/IdentityFunctorsTest.java
+++ src/test/java/org/refact4j/functor/IdentityFunctorsTest.java
@@ -3,8 +3,6 @@ package org.refact4j.functor;
 import static org.junit.Assert.assertSame;
 import org.junit.Test;
 import org.refact4j.functor.identity.Identity;
-import org.refact4j.functor.identity.Project1st;
-import org.refact4j.functor.identity.Project2nd;
 
 
 public class IdentityFunctorsTest {
@@ -14,20 +12,6 @@ public class IdentityFunctorsTest {
         java.util.function.Function<Object,?> identity = new Identity<Object>();
         assertSame(identity, identity.apply(identity));
         assertSame(this, identity.apply(this));
-    }
-
-    @Test
-    public void testProject1st() {
-        BinaryFunctor<Object, Object, Object> project1st = new Project1st<Object, Object>();
-        assertSame(project1st, project1st.eval(project1st, "abc"));
-        assertSame(this, project1st.eval(this, "abc"));
-    }
-
-    @Test
-    public void testProject2nd() {
-        BinaryFunctor<Object, Object, Object> project2nd = new Project2nd<Object, Object>();
-        assertSame(project2nd, project2nd.eval("abc", project2nd));
-        assertSame(this, project2nd.eval("abc", this));
     }
 
 }
