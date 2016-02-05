5340024da8f89cedc3d7337637db6b93c36bfc2d
Vincent Dupain <vdupain@gmail.com>
2016-02-05 11:30:31 +0100

    renaming datasetconverterholder

--- src/main/java/org/refact4j/xml/DatasetConverterHolder.java
+++ src/main/java/org/refact4j/xml/DatasetConverterHolder.java
@@ -1,6 +1,6 @@
 package org.refact4j.xml;
 
-public interface DatasetConverterHolder extends DataSetHolder {
+public interface DatasetConverterHolder extends DatasetHolder {
 
     DataSet2XmlConverter getDataset2XmlConverter();
 
