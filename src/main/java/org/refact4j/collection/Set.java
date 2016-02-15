cafcf1d1292d166e8cb8e73ac892a08e9491d71a
Vincent Dupain <vdupain@gmail.com>
2016-02-15 07:41:54 +0100

    refactoring on Set

--- src/main/java/org/refact4j/collection/Set.java
+++ src/main/java/org/refact4j/collection/Set.java
@@ -1,6 +1,8 @@
 package org.refact4j.collection;
 
 import org.refact4j.core.Finder;
+import org.refact4j.core.IdResolver;
+import org.refact4j.core.TypeResolver;
 
 import java.util.HashSet;
 import java.util.List;
@@ -16,6 +18,16 @@ public class Set<T, ID, TYPE> extends HashSet<T> implements Finder<T, ID, TYPE> 
     public List<T> getAll(final TYPE type) {
         return this.stream().filter(t -> getTypeResolver().isSameType(type, getTypeResolver().getTypeOf(t)))
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public IdResolver<T, ID> getIdResolver() {
+        return null;
+    }
+
+    @Override
+    public TypeResolver<T, TYPE> getTypeResolver() {
+        return null;
     }
 
 }
