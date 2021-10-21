581d7d9d8112595194dd04356b029463977f6fbe
Vincent Dupain <vdupain@gmail.com>
2021-10-21 15:02:12 +0200

    Clean Code

--- src/test/java/org/refact4j/function/FunctionsTest.java
+++ src/test/java/org/refact4j/function/FunctionsTest.java
@@ -5,17 +5,13 @@ import org.refact4j.eom.EntityFieldComparator;
 import org.refact4j.eom.EntityObject;
 import org.refact4j.eom.EntityObjectBuilder;
 import org.refact4j.eom.FieldValuePredicate;
-import org.refact4j.function.arithmetic.Multiplies;
-import org.refact4j.function.comparison.Equal;
-import org.refact4j.function.comparison.Greater;
-import org.refact4j.function.comparison.GreaterEqual;
 import org.refact4j.function.comparison.LessEqual;
 import org.refact4j.function.logical.And;
 import org.refact4j.function.logical.Not;
 import org.refact4j.model.DummyBean;
 import org.refact4j.model.FooDesc;
-import org.refact4j.util.PrettyPrinter;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import static org.junit.Assert.*;
@@ -28,49 +24,54 @@ public class FunctionsTest {
         double discountRate = 0.2;
         double taxRate = 0.20;
 
-        Function<Number, Number> discountedPrice = bind1st(new Multiplies(),
+        Function<Double, Double> discountedPrice = bind1st((o1, o2) -> o1*o2,
                 1 - discountRate);
-        Function<Number, Number> taxedPrice = bind1st(new Multiplies(), 1 + taxRate);
+        Function<Double, Double> taxedPrice = bind1st((o1, o2) -> o1*o2, 1 + taxRate);
 
-        Number value = 100.;
+        double value = 100.;
         value = discountedPrice.apply(value);
-        assertEquals(value, 80.);
+        assertEquals(value, 80., 0);
         value = taxedPrice.apply(value);
-        assertEquals(value, 96.);
+        assertEquals(value, 96., 0);
 
         value = 100.;
         value = discountedPrice.apply(taxedPrice.apply(value));
-        assertEquals(value, 96.);
+        assertEquals(value, 96., 0);
 
-        ComposeFunction<Number, Number, Number> calcPrice = new ComposeFunction<>(
+        ComposeFunction<Double, Double, Double> calcPrice = new ComposeFunction<>(
                 discountedPrice, taxedPrice);
         value = 100.;
         value = calcPrice.apply(value);
-        assertEquals(value, 96.);
+        assertEquals(value, 96., 0);
 
         value = 100.;
         value = discountedPrice.compose(taxedPrice).apply(value);
-        assertEquals(value, 96.);
+        assertEquals(value, 96., 0);
 
-        Function<Number, Number> functor = bind2nd(new Multiplies(), 1 + taxRate);
+        Function<Double, Double> functor = bind2nd((o1, o2) -> o1*o2, 1 + taxRate);
         value = 100.;
         value = discountedPrice.apply(value);
-        assertEquals(value, 80.);
+        assertEquals(value, 80., 0);
         value = functor.apply(value);
-        assertEquals(value, 96.);
+        assertEquals(value, 96., 0);
     }
 
     @Test
     public void testCompositeFunction() {
         BinaryCompose<Boolean, Boolean, Boolean, Boolean> func3 = new BinaryCompose<>(
                 new And(), new Not(), new Not());
+
         assertFalse(func3.apply(true));
 
+
+        BiFunction<Double, Double, Boolean> greater = (o1, o2) -> o1 > o2;
+        BiFunction<Double, Double, Boolean> greaterEquals = (o1, o2) -> o1 >= o2;
+
         java.util.function.Function<org.refact4j.model.DummyBean, Boolean> predicate = new BinaryCompose<>(
-                new Greater(), constant(1.), constant(2.));
+                greater, constant(1.), constant(2.));
         assertFalse(predicate.apply(null));
 
-        predicate = new BinaryCompose<>(new Greater(),
+        predicate = new BinaryCompose<>(greater,
                 new GetFieldFunction("Value"), constant(100.));
         DummyBean dummy = new DummyBean();
         dummy.setValue(99.);
@@ -84,14 +85,13 @@ public class FunctionsTest {
                     .getMessage());
         }
 
-        PrettyPrinter printer = new PrettyPrinter();
-        EntityFieldComparator<Double> fieldComparator = new EntityFieldComparator<>(new Greater(), FooDesc.VALUE,
+        EntityFieldComparator<Double> fieldComparator = new EntityFieldComparator<>(greater, FooDesc.VALUE,
                 100.);
         EntityObject dummyEntity = EntityObjectBuilder.init(FooDesc.INSTANCE).set(FooDesc.VALUE,
                 99.).get();
         assertFalse(fieldComparator.test(dummyEntity));
 
-        fieldComparator = new EntityFieldComparator<>(new GreaterEqual(), FooDesc.VALUE, 12.);
+        fieldComparator = new EntityFieldComparator<>(greaterEquals, FooDesc.VALUE, 12.);
         dummyEntity = EntityObjectBuilder.init(FooDesc.INSTANCE).set(FooDesc.VALUE, 12.).get();
         assertTrue(fieldComparator.test(dummyEntity));
 
@@ -103,9 +103,7 @@ public class FunctionsTest {
                 .get();
         assertFalse(dummyWith100AsValue.apply(dummyEntity));
 
-        System.out.println(">>" + printer.toString(dummyWith100AsValue));
-
-        EntityFieldComparator<Double> fc1 = new EntityFieldComparator<>(new GreaterEqual(), FooDesc.VALUE, 10.);
+        EntityFieldComparator<Double> fc1 = new EntityFieldComparator<>(greaterEquals, FooDesc.VALUE, 10.);
         EntityFieldComparator<?> fc2 = new EntityFieldComparator<>(new LessEqual(), FooDesc.VALUE, 15.);
 
         BinaryCompose<Boolean, Boolean, EntityObject, Boolean> func4 = new BinaryCompose<>(
@@ -115,8 +113,10 @@ public class FunctionsTest {
         dummyEntity = EntityObjectBuilder.init(FooDesc.INSTANCE).set(FooDesc.VALUE, 9.).get();
         assertFalse(func4.apply(dummyEntity));
 
-        fc1 = new EntityFieldComparator<>(new GreaterEqual(), FooDesc.VALUE, 10.);
-        fc2 = new EntityFieldComparator<>(new Equal(), FooDesc.NAME, "azerty");
+        fc1 = new EntityFieldComparator<>(greaterEquals, FooDesc.VALUE, 10.);
+        BiFunction<String, String, Boolean> isEquals = (o1, o2) -> o1.equals(o2);
+
+        fc2 = new EntityFieldComparator<>(isEquals, FooDesc.NAME, "azerty");
         func4 = new BinaryCompose<>(new And(), fc1, fc2);
         dummyEntity = EntityObjectBuilder.init(FooDesc.INSTANCE).set(FooDesc.VALUE, 12.)
                 .set(FooDesc.NAME, "azerty").get();
